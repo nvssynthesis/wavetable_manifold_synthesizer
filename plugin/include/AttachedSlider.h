c32903b1b2c724dd1b6b173713cbc3e815aee882
Nicholas Solem <nsolem@ucsd.edu>
2024-10-25 16:10:22 +0800

    separate implementation from declaration for attached slider

--- plugin/include/AttachedSlider.h
+++ plugin/include/AttachedSlider.h
@@ -7,24 +7,9 @@ struct AttachedSlider {
     using SliderAttachment = juce::AudioProcessorValueTreeState::SliderAttachment;
 
     AttachedSlider(juce::AudioProcessorValueTreeState &apvts,
-        params::params_e const param,
-        Slider::SliderStyle const sliderStyle = Slider::SliderStyle::RotaryHorizontalVerticalDrag,
-        juce::Slider::TextEntryBoxPosition const entryPos = juce::Slider::TextBoxBelow)
-    // AttachedSlider(juce::RangedAudioParameter* rap,
-    //     Slider::SliderStyle  style = Slider::SliderStyle::RotaryHorizontalVerticalDrag,
-    //     juce::Slider::TextEntryBoxPosition  entryPos = juce::Slider::TextBoxBelow)
-    :
-    slider_(sliderStyle, entryPos),
-    attachment_(apvts, params::get_param_id(param), slider_)
-    {
-        slider_.setSliderStyle(sliderStyle);
-        slider_.setNormalisableRange(params::get_normalizable_range<double>(param));
-        slider_.setTextBoxStyle(entryPos, false, 50, 25);
-        slider_.setValue(/*getParamDefault(param)*/ 0.0);
-
-        slider_.setColour(Slider::ColourIds::thumbColourId, juce::Colours::palevioletred);
-        slider_.setColour(Slider::ColourIds::textBoxTextColourId, juce::Colours::lightgrey);
-    }
+        params::params_e param,
+        Slider::SliderStyle sliderStyle = Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+        juce::Slider::TextEntryBoxPosition entryPos = juce::Slider::TextBoxBelow);
 
     Slider slider_;
     SliderAttachment attachment_;
---
+++ plugin/source/AttachedSlider.cpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "AttachedSlider.h"
+
+AttachedSlider::AttachedSlider(juce::AudioProcessorValueTreeState &apvts,
+        params::params_e const param,
+        Slider::SliderStyle const sliderStyle,
+        juce::Slider::TextEntryBoxPosition const entryPos)
+:
+slider_(sliderStyle, entryPos),
+attachment_(apvts, params::get_param_id(param), slider_)
+{
+    slider_.setSliderStyle(sliderStyle);
+    slider_.setNormalisableRange(params::get_normalizable_range<double>(param));
+    slider_.setTextBoxStyle(entryPos, false, 50, 25);
+    slider_.setValue(/*getParamDefault(param)*/ 0.0);
+
+    slider_.setColour(Slider::ColourIds::thumbColourId, juce::Colours::palevioletred);
+    slider_.setColour(Slider::ColourIds::textBoxTextColourId, juce::Colours::lightgrey);
+}
+
